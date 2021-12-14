1a231ba6c2cd9a92a0c4c42f23d935e4d4c91c66
Stefany Izidio <stefanyvitoria9307@gmail.com>
2021-12-13 22:03:26 -0300

    - add navheader.

---
+++ src/components/navheader/NavHeader.js
@@ -0,0 +1,18 @@
+import './NavHeader.css'
+
+export default function NavHeader() {
+    return(
+    <nav className='navheader'>
+        <div className='container'>
+            <div className='navheader-menus'>
+                <p>Olá</p>
+                <p>Sobre mim</p>
+                <p>Projetos</p>
+                <p>Skills</p>
+                <p>Contato</p>
+            </div>
+
+        </div>
+    </nav>
+    );
+}
